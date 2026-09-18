5a92aa005670294dd1fb2c2561dc5c5f77c3e6d6
Bhargav_D <devarampativenkata.bhargav@invendis.com>
2026-09-18 12:59:16 +0530

    loading the files from .env

---
+++ backend/app/config.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from pydantic_settings import SettingsConfigDict, BaseSettings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+class Settings(BaseSettings):
+    database_url: str
+
+    model_config = SettingsConfigDict(
+                    env_file=BASE_DIR / ".env",
+                    env_file_encoding="utf-8",
+                    extra="ignore")
+
+
+settings = Settings()
