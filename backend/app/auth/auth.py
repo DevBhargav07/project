fe0648dd1a116d37aafe03c91072c79c50e02211
DevBhargav07 <249870767+DevBhargav07@users.noreply.github.com>
2026-09-19 20:04:54 +0530

    changing the Oauthlogin url for the correct url to match it

--- backend/app/auth/auth.py
+++ backend/app/auth/auth.py
@@ -21,7 +21,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 2 # 2 HOURS
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
