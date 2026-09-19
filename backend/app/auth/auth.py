523b9e2ec94d55dada1b4a47a9a6b61673ddf65f
DevBhargav07 <249870767+DevBhargav07@users.noreply.github.com>
2026-09-20 00:09:11 +0530

    adding authentication pages as matrix theme

--- backend/app/auth/auth.py
+++ backend/app/auth/auth.py
@@ -4,10 +4,12 @@ from typing import Optional
 from typing_extensions import Annotated
 from jose import JWTError, jwt
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from fastapi import APIRouter
+from app.schemas import UserOut, UserCreate, LoginResponse
 
 from app.database import get_async_session, AsyncSessionLocal
 from app.models import User
@@ -19,6 +21,8 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")  # Replace with your actual secret key
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 2 # 2 HOURS
+
+session_dependency = Annotated[AsyncSession, Depends(get_async_session)]
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
@@ -64,4 +68,80 @@ async def get_current_user(
         return result
 
 
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+)
 
+
+@router.post("/register", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+async def create_user(user_create: UserCreate, session: session_dependency):
+    # checking email exists
+    user_details = select(User).where(User.email == user_create.email)
+    if await session.scalar(user_details):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+
+    # checking username exists
+    user_details = select(User).where(User.username == user_create.username)
+    if await session.scalar(user_details):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already taken")
+
+    db_user = User(
+        email = user_create.email,
+        username = user_create.username,
+        password = User.hash_password(user_create.password)
+    )
+
+    session.add(db_user)
+    try:
+        await session.commit()
+    except IntegrityError:
+        await session.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email or Username already exists")
+    await session.refresh(db_user)
+    return db_user
+
+# @router.get("/users", response_model=List[UserOut])
+# async def get_users(session: session_dependency, skip: int = 0, limit: int = 30):
+#     result = await session.scalars(select(User).offset(skip).limit(limit))
+#     users = result.all()
+#     return [UserOut.model_validate(user) for user in users]
+
+# @router.get("/{user_id}", response_model=UserOut)
+# async def get_user(user_id: int, session: session_dependency):
+#     user = await session.scalar(select(User).where(User.id == user_id))
+#     if not user:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+#     return user
+
+@router.post("/login", response_model=LoginResponse)
+async def login_user(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: session_dependency
+):
+    """ 
+    Login with username and password
+    Retures a JWT token to use in the Authorization header.
+    """
+
+    user_details = select(User).where(User.username == form_data.username)
+    user = await session.scalar(user_details)
+    if not user or not user.verify_password(form_data.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Invalid username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    if not user.is_active:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User account is inactive")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": str(user.id)}, expires_delta=access_token_expires
+    )
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user_id": user.id,
+        "is_superuser": user.is_superuser
+    }
