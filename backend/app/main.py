8875c77bf0f333b8d4b503eed07144cf0b852417
Bhargav_D <devarampativenkata.bhargav@invendis.com>
2026-09-25 18:06:58 +0530

    adding new trusted origin with port

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app = FastAPI(title="Project Backend Learning", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:4173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
